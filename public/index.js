244169afd6dd357f62ee442f1f756af7e48ffd73
Paolo Scanferla <paolo.scanferla@mondora.com>
2014-07-11 13:05:31 +0200

    Step 2

--- public/index.js
+++ public/index.js
@@ -5,18 +5,48 @@ document.addEventListener("DOMContentLoaded", function () {
 	var addTodoButton = document.getElementById("addTodo");
 	var todoTextInput = document.getElementById("todoText");
 
+	var addTodoToList = function (todo) {
+		// Create a new li element
+		var newTodoLi = document.createElement("li");
+		// Set the text of the li element
+		newTodoLi.textContent = todo;
+		// Append the element to the list
+		listContainerUl.appendChild(newTodoLi);
+	};
+
+	var addTodoToLocasStorage = function (todo) {
+		// Get the current array from localStorage
+		var list = getListFromLocalStorage();
+		list.push(todo);
+		localStorage.list = JSON.stringify(list);
+	};
+
+	var getListFromLocalStorage = function () {
+		var list;
+		try {
+			list = JSON.parse(localStorage.list);
+		} catch (e) {
+			list = [];
+		}
+		return list;
+	};
+
 	// Register listeners for the click event
 	addTodoButton.addEventListener("click", function () {
 		// Get the text in the input
 		var text = todoTextInput.value;
 		// Clear the input
 		todoTextInput.value = "";
-		// Create a new li element
-		var newTodoLi = document.createElement("li");
-		// Set the text of the li element
-		newTodoLi.textContent = text;
-		// Append the element to the list
-		listContainerUl.appendChild(newTodoLi);
+		// Add the todo to the list
+		addTodoToList(text);
+		// Add the todo to localStorage
+		addTodoToLocasStorage(text);
+	});
+
+	// Load todo-s from localStorage
+	var list = getListFromLocalStorage();
+	list.forEach(function (todo) {
+		addTodoToList(todo);
 	});
 
 });
